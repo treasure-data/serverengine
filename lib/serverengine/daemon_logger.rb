483f20aaa014f451c27721c75c3c13c01e6ee39f
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2013-10-18 17:28:21 -0700

    DaemonLogger: fixed log rotation [#9]

--- lib/serverengine/daemon_logger.rb
+++ lib/serverengine/daemon_logger.rb
@@ -20,42 +20,49 @@ module ServerEngine
   require 'logger'
 
   class DaemonLogger < Logger
-    def initialize(dev, config={})
-      @hook_stdout = config.fetch(:log_stdout, true)
-      @hook_stderr = config.fetch(:log_stderr, true)
+    def initialize(path_or_io, config={})
       rotate_age = config[:log_rotate_age] || 5
       rotate_size = config[:log_rotate_size] || 1048576
 
-      if dev.is_a?(String)
-        @path = dev
-        @io = File.open(@path, "a")
-        @io.sync = true
-      else
-        @io = dev
-      end
+      @file_dev = MultiprocessFileLogDevice.new(nil,
+        shift_age: rotate_age, shift_size: rotate_size)
 
-      hook_stdout! if @hook_stdout
-      hook_stderr! if @hook_stderr
-
-      super(@io, rotate_age, rotate_size)
+      super(nil)
 
       self.level = config[:log_level] || 'debug'
+
+      # overwrite @logdev
+      if path_or_io.respond_to?(:write) and path_or_io.respond_to?(:close)
+        # IO
+        @logdev = path_or_io
+        @logdev.sync = true if @logdev.respond_to?(:sync=)
+      else
+        # path
+        @file_dev.path = path_or_io
+        @logdev = @file_dev
+      end
     end
 
-    attr_accessor :path
+    def path=(path)
+      @file_dev.path = path
+      @logdev = @file_dev
+      path
+    end
+
+    attr_reader :path
 
     def level=(expr)
       case expr.to_s
-      when 'fatal', Logger::FATAL.to_s
-        e = Logger::FATAL
-      when 'error', Logger::ERROR.to_s
-        e = Logger::ERROR
-      when 'warn', Logger::WARN.to_s
-        e = Logger::WARN
-      when 'info', Logger::INFO.to_s
-        e = Logger::INFO
-      when 'debug', Logger::DEBUG.to_s
-        e = Logger::DEBUG
+      when 'fatal', FATAL.to_s
+        e = FATAL
+      when 'error', ERROR.to_s
+        e = ERROR
+      when 'warn', WARN.to_s
+        e = WARN
+      when 'info', INFO.to_s
+        e = INFO
+      when 'debug', DEBUG.to_s
+        e = DEBUG
       else
         raise ArgumentError, "invalid log level: #{expr}"
       end
@@ -63,28 +70,10 @@ module ServerEngine
       super(e)
     end
 
-    def hook_stdout!
-      STDOUT.sync = true
-      @hook_stdout = true
-
-      STDOUT.reopen(@io) if @io != STDOUT
-      self
-    end
-
-    def hook_stderr!
-      STDERR.sync = true
-      @hook_stderr = true
-
-      STDERR.reopen(@io) if @io != STDERR
-      self
-    end
-
     def reopen!
       if @path
         @io.reopen(@path, "a")
         @io.sync = true
-        hook_stdout! if @hook_stdout
-        hook_stderr! if @hook_stderr
       end
       nil
     end
@@ -104,6 +93,100 @@ module ServerEngine
         @io.close unless @io.closed?
       end
       nil
+    end
+
+    class MultiprocessFileLogDevice
+      def initialize(path, opts={})
+        @shift_age = opts[:shift_age] || 7
+        @shift_size = opts[:shift_size] || 1024*1024
+        @rotate_mutex = Mutex.new
+        self.path = path
+      end
+
+      def write(message)
+        return nil unless @file
+        if @file.size > @shift_size
+          log_rotate_or_reopen
+        end
+        @file.write message
+      rescue Exception => e
+        warn("log writing failed. #{e}")
+      end
+
+      def path=(path)
+        @rotate_mutex.synchronize do
+          old_file = @file
+          file = open_logfile(path)
+          begin
+            @file = file
+            @path = path
+            file = old_file
+          ensure
+            file.close if file
+          end
+        end
+        path
+      end
+
+      def close
+        @rotate_mutex.synchronize do
+          @file.close
+          @file = nil
+        end
+        nil
+      end
+
+      attr_reader :path
+
+      def reopen!
+        @file.reopen(@path, 'a')
+        @file.sync = true
+        true
+      end
+
+      private
+
+      def open_logfile(path)
+        return nil unless path
+        file = File.open(path, 'a')
+        file.sync = true
+        # Logger::LogDevice writes header to the file if file is empty.
+        return file
+      end
+
+      def log_rotate_or_reopen
+        @rotate_mutex.synchronize do
+          return unless @file
+
+          # double-checked locking
+          return if @file.size <= @shift_size
+
+          # inter-process locking
+          @file.flock(File::LOCK_EX)
+          begin
+            ino = @file.stat.ino
+            if File.stat(@path).ino == ino
+              # lock succeeded
+              log_rotate
+            else
+              # other process locked
+              reopen!
+            end
+          ensure
+            @file.flock(File::LOCK_UN)
+          end
+        end
+      end
+
+      def log_rotate
+        (@shift_age-2).downto(0) do |i|
+          if FileTest.exist?("#{@path}.#{i}")
+            File.rename("#{@path}.#{i}", "#{@path}.#{i+1}")
+          end
+        end
+        File.rename("#{@path}", "#{@path}.0")
+        reopen!
+      end
     end
   end
 
--- spec/daemon_logger_spec.rb
+++ spec/daemon_logger_spec.rb
@@ -1,40 +1,26 @@
+require 'stringio'
 
 describe ServerEngine::DaemonLogger do
   before { FileUtils.mkdir_p("tmp") }
   before { FileUtils.rm_f("tmp/se1.log") }
+  before { FileUtils.rm_f Dir["tmp/se1.log.**"] }
   before { FileUtils.rm_f("tmp/se2.log") }
 
-  subject { DaemonLogger.new("tmp/se1.log", log_stdout: false, log_stderr: false) }
+  subject { DaemonLogger.new("tmp/se1.log", level: 'info') }
 
   it 'reopen' do
+    subject.warn "ABCDEF"
+    File.open('tmp/se1.log', "w") {|f| }
+    subject.warn "test2"
+
+    File.read('tmp/se1.log').should_not =~ /ABCDEF/
+  end
+
+  it 'reset path' do
     subject.path = 'tmp/se2.log'
-    subject.reopen!
     subject.warn "test"
 
     File.read('tmp/se2.log').should =~ /test$/
-  end
-
-  it 'stderr hook 1' do
-    subject.hook_stderr!
-    STDERR.puts "test"
-
-    File.read('tmp/se1.log').should == "test\n"
-  end
-
-  it 'stderr hook 2' do
-    log = DaemonLogger.new("tmp/se1.log", log_stdout: false, log_stderr: true)
-    STDERR.puts "test"
-
-    File.read('tmp/se1.log').should == "test\n"
-  end
-
-  it 'stderr hook and reopen' do
-    subject.hook_stderr!
-    subject.path = 'tmp/se2.log'
-    subject.reopen!
-    STDERR.puts "test"
-
-    File.read('tmp/se2.log').should == "test\n"
   end
 
   it 'default level is debug' do
@@ -45,18 +31,43 @@ describe ServerEngine::DaemonLogger do
   it 'level set by int' do
     subject.level = Logger::FATAL
     subject.level.should == Logger::FATAL
+    subject.debug?.should == false
+    subject.info?.should  == false
+    subject.warn?.should  == false
+    subject.error?.should == false
+    subject.fatal?.should == true
 
     subject.level = Logger::ERROR
     subject.level.should == Logger::ERROR
+    subject.debug?.should == false
+    subject.info?.should  == false
+    subject.warn?.should  == false
+    subject.error?.should == true
+    subject.fatal?.should == true
 
     subject.level = Logger::WARN
     subject.level.should == Logger::WARN
+    subject.debug?.should == false
+    subject.info?.should  == false
+    subject.warn?.should  == true
+    subject.error?.should == true
+    subject.fatal?.should == true
 
     subject.level = Logger::INFO
     subject.level.should == Logger::INFO
+    subject.debug?.should == false
+    subject.info?.should  == true
+    subject.warn?.should  == true
+    subject.error?.should == true
+    subject.fatal?.should == true
 
     subject.level = Logger::DEBUG
     subject.level.should == Logger::DEBUG
+    subject.debug?.should == true
+    subject.info?.should  == true
+    subject.warn?.should  == true
+    subject.error?.should == true
+    subject.fatal?.should == true
   end
 
   it 'level set by string' do
@@ -80,9 +91,34 @@ describe ServerEngine::DaemonLogger do
     lambda { subject.level = 'unknown' }.should raise_error(ArgumentError)
   end
 
-  it 'stdout logger' do
-    STDOUT.should_not_receive(:reopen)
-    log = DaemonLogger.new(STDOUT)
+  it 'rotation' do
+    log = DaemonLogger.new("tmp/se1.log", level: 'info', log_rotate_age: 3, log_rotate_size: 10)
+    log.warn "test1"
+    File.exist?("tmp/se1.log").should == true
+    File.exist?("tmp/se1.log.0").should == false
+
+    log.warn "test2"
+    File.exist?("tmp/se1.log").should == true
+    File.exist?("tmp/se1.log.0").should == true
+    File.read("tmp/se1.log.0") =~ /test2$/
+
+    log.warn "test3"
+    log.warn "test4"
+    File.exist?("tmp/se1.log").should == true
+    File.exist?("tmp/se1.log.2").should == true
+    File.exist?("tmp/se1.log.3").should == false
+
+    log.warn "test5"
+    File.read("tmp/se1.log.0") =~ /test5$/
+  end
+
+  it 'IO logger' do
+    io = StringIO.new
+    io.should_receive(:write)
+    io.should_not_receive(:reopen)
+
+    log = DaemonLogger.new(io)
     log.debug "stdout logging test"
+    log.reopen!
   end
 end
