28780dc05f72140b9bbc81d2860a960ad39da7f9
RittaNarita <narittan@gmail.com>
2015-09-24 13:52:06 +0900

    Add Socket Manager

---
+++ lib/serverengine/socket_manager.rb
@@ -0,0 +1,112 @@
+#
+# ServerEngine
+#
+# Copyright (C) 2012-2013 Sadayuki Furuhashi
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+require 'socket'
+require 'fcntl'
+
+module ServerEngine
+  module SocketManager
+
+    def self.new_socket_manager
+      uds_drb = ARGV[-1]
+      puts uds_drb
+      unix_socket_client = UNIXSocket.for_fd(uds_drb.split('#').first.to_i)
+      drb_uri = uds_drb.split('#').last
+      # if usc isn't int or drb isnt incluging str druby, generate error
+      SocketManager::Client.new(unix_socket_client, drb_uri)
+    end
+
+    class Client
+      def initialize(unix_socket_client, drb_uri)
+        @unix_socket_client = unix_socket_client
+        @sm_server = DRb::DRbObject.new_with_uri(drb_uri)
+      end
+
+      def get_udp(bind, port)
+        @sm_server.udp_socket_fd(bind, port)
+        @unix_socket_client.recv_io
+      end
+
+      def get_tcp(bind, port)
+        @sm_server.socket_fd(bind, port)
+        @unix_socket_client.recv_io
+      end
+    end
+
+    class Server
+      def initialize
+        @tcp_socks = {}
+        @udp_socks = {}
+        @unix_socket_server = nil
+      end
+
+      def new_unix_socket
+        @unix_socket_server, unix_socket_client = UNIXSocket.pair
+        unix_socket_client
+      end
+
+      def socket_fd(bind, port)
+
+        socks_key = bind.to_s + port.to_s
+
+        if @tcp_socks.has_key?(socks_key)
+          @unix_socket_server.send_io @tcp_socks[socks_key]
+        else
+          sock = nil
+          begin
+            sock = TCPServer.new(bind, port)
+            sock.setsockopt(:SOCKET, :REUSEADDR, true)
+            sock.listen(Socket::SOMAXCONN)
+          rescue
+            "failed to listen to #{bind_port}"
+          end
+          sock.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
+
+          @tcp_socks[socks_key] = sock
+          @unix_socket_server.send_io @tcp_socks[socks_key]
+        end
+      end
+
+      def udp_socket_fd(bind, port)
+
+        socks_key = bind.to_s + port.to_s
+
+        if @udp_socks.has_key?(socks_key)
+          @unix_socket_server.send_io @udp_socks[socks_key]
+        else
+          sock = nil
+          begin
+            if IPAddr.new(IPSocket.getaddress(bind)).ipv4?
+              sock = UDPSocket.new
+            else
+              sock = UDPSocket.new(Socket::AF_INET6)
+            end
+            sock.bind(bind, port)
+          rescue
+            "failed to create udp on #{bind_port}"
+          end
+          sock.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
+
+          @udp_socks[socks_key] = sock
+          @unix_socket_server.send_io @udp_socks[socks_key]
+        end
+      end
+
+    end
+  end
+end
