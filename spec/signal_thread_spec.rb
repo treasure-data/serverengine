59fdeafef1387842ce85a6a79d4f07b8f76d369c
U.Nakamura <usa@garbagecollect.jp>
2016-07-29 20:29:09 +0900

    Windows has no signal which has no side effect

--- spec/signal_thread_spec.rb
+++ spec/signal_thread_spec.rb
@@ -6,84 +6,89 @@ describe ServerEngine::SignalThread do
     t.join
   end
 
-  it 'call handler' do
-    n = 0
+  unless ServerEngine.windows?
+    it 'call handler' do
+      n = 0
 
-    t = SignalThread.new do |st|
-      st.trap('CONT') { n += 1 }
+      t = SignalThread.new do |st|
+        st.trap('CONT') { n += 1 }
+      end
+
+      Process.kill('CONT', Process.pid)
+      sleep 0.5
+
+      t.stop.join
+
+      n.should == 1
     end
 
-    Process.kill('CONT', Process.pid)
-    sleep 0.5
+    it 'SIG_IGN' do
+      # IGNORE signal handler has possible race condition in Ruby 2.1
+      # https://bugs.ruby-lang.org/issues/9835
+      # That's why ignore this test in Ruby2.1
+      if RUBY_VERSION >= '2.2'
+        t = SignalThread.new do |st|
+          st.trap('QUIT', 'SIG_IGN')
+        end
 
-    t.stop.join
+        Process.kill('QUIT', Process.pid)
 
-    n.should == 1
-  end
+        t.stop.join
+      end
+    end
 
-  it 'SIG_IGN' do
-    # IGNORE signal handler has possible race condition in Ruby 2.1
-    # https://bugs.ruby-lang.org/issues/9835
-    # That's why ignore this test in Ruby2.1
-    if RUBY_VERSION >= '2.2'
-      t = SignalThread.new do |st|
-        st.trap('QUIT', 'SIG_IGN')
+    it 'signal in handler' do
+      n = 0
+
+      SignalThread.new do |st|
+        st.trap('QUIT') do
+          if n < 3
+            Process.kill('QUIT', Process.pid)
+            n += 1
+          end
+        end
       end
 
       Process.kill('QUIT', Process.pid)
+      sleep 0.5
 
-      t.stop.join
+      n.should == 3
     end
-  end
 
-  it 'signal in handler' do
-    n = 0
-
-    SignalThread.new do |st|
-      st.trap('QUIT') do
-        if n < 3
-          Process.kill('QUIT', Process.pid)
-          n += 1
-        end
+    it 'stop in handler' do
+      t = SignalThread.new do |st|
+        st.trap('QUIT') { st.stop }
       end
-    end
 
-    Process.kill('QUIT', Process.pid)
-    sleep 0.5
+      Process.kill('QUIT', Process.pid)
+      sleep 0.5
 
-    n.should == 3
-  end
-
-  it 'stop in handler' do
-    t = SignalThread.new do |st|
-      st.trap('QUIT') { st.stop }
-    end
-
-    Process.kill('QUIT', Process.pid)
-    sleep 0.5
-
-    t.join
-  end
-
-  it 'should not deadlock' do
-    n = 0
-
-    SignalThread.new do |st|
-      st.trap('CONT') { n += 1 }
-    end
-
-    (1..10).map {
-      Thread.new do
-        10.times {
-          Process.kill('CONT', Process.pid)
-        }
-      end
-    }.each { |t|
       t.join
-    }
+    end
 
-    # result won't be 100 because of kernel limitation
-    n.should > 0
+    it 'should not deadlock' do
+      n = 0
+
+      SignalThread.new do |st|
+        st.trap('CONT') { n += 1 }
+      end
+
+      (1..10).map {
+        Thread.new do
+          10.times {
+            Process.kill('CONT', Process.pid)
+          }
+        end
+      }.each { |t|
+        t.join
+      }
+
+      # give chance to run the signal thread
+      sleep 0.1
+
+      # result won't be 100 because of kernel limitation
+      n.should > 0
+    end
   end
 end
 
