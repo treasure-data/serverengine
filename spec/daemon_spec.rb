fa63c60f44b53dc7ad148c28e2fba8a81950ad4a
U.Nakamura <usa@garbagecollect.jp>
2016-08-16 10:34:56 +0900

    Use `pending`

--- spec/daemon_spec.rb
+++ spec/daemon_spec.rb
@@ -2,52 +2,53 @@
 describe ServerEngine::Daemon do
   include_context 'test server and worker'
 
-  unless ServerEngine.windows?
-    it 'run and graceful stop by signal' do
-      dm = Daemon.new(TestServer, TestWorker, daemonize: true, pid_path: "tmp/pid")
-      dm.main
+  it 'run and graceful stop by signal' do
+    pending "not supported signal base commands on Windows" if ServerEngine.windows?
 
-      wait_for_fork
+    dm = Daemon.new(TestServer, TestWorker, daemonize: true, pid_path: "tmp/pid")
+    dm.main
 
-      test_state(:server_initialize).should == 1
+    wait_for_fork
 
-      begin
-        dm.stop(true)
-        wait_for_stop
+    test_state(:server_initialize).should == 1
 
-        test_state(:server_stop_graceful).should == 1
-        test_state(:worker_stop).should == 1
-        test_state(:server_after_run).should == 1
-      ensure
-        dm.stop(false) rescue nil
-      end
+    begin
+      dm.stop(true)
+      wait_for_stop
+
+      test_state(:server_stop_graceful).should == 1
+      test_state(:worker_stop).should == 1
+      test_state(:server_after_run).should == 1
+    ensure
+      dm.stop(false) rescue nil
     end
+  end
 
-    it 'signals' do
-      dm = Daemon.new(TestServer, TestWorker, daemonize: true, pid_path: "tmp/pid")
-      dm.main
+  it 'signals' do
+    pending "not supported signal base commands on Windows" if ServerEngine.windows?
+    dm = Daemon.new(TestServer, TestWorker, daemonize: true, pid_path: "tmp/pid")
+    dm.main
 
-      wait_for_fork
+    wait_for_fork
 
-      begin
-        dm.reload
-        wait_for_stop
-        test_state(:server_reload).should == 1
+    begin
+      dm.reload
+      wait_for_stop
+      test_state(:server_reload).should == 1
 
-        dm.restart(true)
-        wait_for_stop
-        test_state(:server_restart_graceful).should == 1
+      dm.restart(true)
+      wait_for_stop
+      test_state(:server_restart_graceful).should == 1
 
-        dm.restart(false)
-        wait_for_stop
-        test_state(:server_restart_immediate).should == 1
+      dm.restart(false)
+      wait_for_stop
+      test_state(:server_restart_immediate).should == 1
 
-        dm.stop(false)
-        wait_for_stop
-        test_state(:server_stop_immediate).should == 1
-      ensure
-        dm.stop(true) rescue nil
-      end
+      dm.stop(false)
+      wait_for_stop
+      test_state(:server_stop_immediate).should == 1
+    ensure
+      dm.stop(true) rescue nil
     end
   end
 
