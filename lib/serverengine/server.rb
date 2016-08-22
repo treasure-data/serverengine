082fe2f275489861acefd2c43f4e0a42e728a0f0
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-08-22 18:36:14 +0900

    fix typo

--- lib/serverengine/server.rb
+++ lib/serverengine/server.rb
@@ -15,7 +15,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
-require 'serverengine/signal'
+require 'serverengine/signals'
 require 'serverengine/signal_thread'
 require 'serverengine/worker'
 
