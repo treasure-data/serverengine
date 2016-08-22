3891a27d467e66fc4ce638e5b358e5ae66a91477
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-08-22 17:07:38 +0900

    remove LogDevice implementation (Ruby 2.1 or later have fixed implementation)

--- lib/serverengine/daemon_logger.rb
+++ lib/serverengine/daemon_logger.rb
@@ -34,14 +34,6 @@ module ServerEngine
       @rotate_size = config[:log_rotate_size] || 1048576
       @file_dev = nil
 
-      if RUBY_VERSION < "2.1.0"
-        # Ruby < 2.1.0 has a problem around log rotation with multiprocess:
-        # https://github.com/ruby/ruby/pull/428
-        @logdev_class = MultiprocessFileLogDevice
-      else
-        @logdev_class = LogDevice
-      end
-
       super(nil)
 
       self.level = config[:log_level] || 'debug'
@@ -62,7 +54,7 @@ module ServerEngine
       elsif !@file_dev || @file_dev.filename != logdev
         # update path string
         old_file_dev = @file_dev
-        @file_dev = @logdev_class.new(logdev, shift_age: @rotate_age, shift_size: @rotate_size)
+        @file_dev = LogDevice.new(logdev, shift_age: @rotate_age, shift_size: @rotate_size)
         old_file_dev.close if old_file_dev
         @logdev = @file_dev
       end
@@ -138,134 +130,6 @@ module ServerEngine
       nil
     end
 
-    class MultiprocessFileLogDevice
-      def initialize(path, opts={})
-        @shift_age = opts[:shift_age] || 7
-        @shift_size = opts[:shift_size] || 1024*1024
-        @mutex = Mutex.new
-        self.path = path
-      end
-
-      def write(data)
-        # it's hard to remove this synchronize because IO#write raises
-        # Errno::ENOENT if IO#reopen is running concurrently.
-        @mutex.synchronize do
-          unless @file
-            return nil
-          end
-          log_rotate_or_reopen
-          @file.write(data)
-        end
-      rescue Exception => e
-        warn "log writing failed: #{e}"
-      end
-
-      def path=(path)
-        @mutex.synchronize do
-          old_file = @file
-          file = open_logfile(path)
-          begin
-            @file = file
-            @path = path
-            file = old_file
-          ensure
-            file.close if file
-          end
-        end
-        return path
-      end
-
-      def close
-        @mutex.synchronize do
-          @file.close
-          @file = nil
-        end
-        nil
-      end
-
-      def reopen!
-        @mutex.synchronize do
-          if @file
-            @file.reopen(@path, 'a')
-            @file.sync = true
-          end
-        end
-        true
-      end
-
-      # for compatibility with Logger::LogDevice
-      def dev
-        @file
-      end
-
-      # for compatibility with Logger::LogDevice
-      def filename
-        @path
-      end
-
-      private
-
-      def open_logfile(path)
-        return nil unless path
-        file = File.open(path, 'a')
-        file.sync = true
-        return file
-      end
-
-      def log_rotate_or_reopen
-        stat = @file.stat
-        if stat.size <= @shift_size
-          return
-        end
-
-        # inter-process locking
-        retry_limit = 8
-        retry_sleep = 0.1
-        begin
-          # 1) other process is log-rotating now
-          # 2) other process log rotated
-          # 3) no active processes
-          lock = File.open(@path, File::WRONLY | File::APPEND)
-          begin
-            lock.flock(File::LOCK_EX)
-            ino = lock.stat.ino
-            if ino == File.stat(@path).ino and ino == stat.ino
-              # 3)
-              log_rotate
-            else
-              @file.reopen(@path, 'a')
-              @file.sync = true
-            end
-          ensure
-            lock.close
-          end
-        rescue Errno::ENOENT => e
-          raise e if retry_limit <= 0
-          sleep retry_sleep
-          retry_limit -= 1
-          retry_sleep *= 2
-          retry
-        end
-
-      rescue => e
-        warn "log rotation inter-process lock failed: #{e}"
-      end
-
-      def log_rotate
-        (@shift_age-2).downto(0) do |i|
-          old_path = "#{@path}.#{i}"
-          shift_path = "#{@path}.#{i+1}"
-          if FileTest.exist?(old_path)
-            File.rename(old_path, shift_path)
-          end
-        end
-        File.rename(@path, "#{@path}.0")
-        @file.reopen(@path, 'a')
-        @file.sync = true
-      rescue => e
-        warn "log rotation failed: #{e}"
-      end
-    end
   end
 
 end
