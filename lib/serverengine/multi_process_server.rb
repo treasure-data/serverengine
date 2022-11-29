87b1adb283113770409cd810f3651795c073467b
Daijiro Fukuda <fukuda@clear-code.com>
2022-11-29 18:36:35 +0900

    Treat worker unexpected shutdown as Warn log level
    This log level was changed to ERROR by the fix: 4d6dbd3d1b4e1af88cc2593b5b19cf0f6da7d5dd.
    However, this log is outputted when using SIGHUP, so ERROR is too strong.
    Leave it as Warn as an adjustment for now.
    
    Before
    
      2022-11-29 18:47:47 +0900 [error]: fluent/log.rb:372:error: Worker 0 finished unexpectedly with status 0
    
    After
    
      2022-11-29 18:45:42 +0900 [warn]: fluent/log.rb:351:warn: Worker 0 finished unexpectedly with status 0
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- lib/serverengine/multi_process_server.rb
+++ lib/serverengine/multi_process_server.rb
@@ -147,7 +147,7 @@ module ServerEngine
           if @stop
             @worker.logger.info "Worker #{@wid} finished with #{ServerEngine.format_join_status(stat)}"
           else
-            @worker.logger.error "Worker #{@wid} finished unexpectedly with #{ServerEngine.format_join_status(stat)}"
+            @worker.logger.warn "Worker #{@wid} finished unexpectedly with #{ServerEngine.format_join_status(stat)}"
           end
           if stat.is_a?(Process::Status) && stat.exited? && @unrecoverable_exit_codes.include?(stat.exitstatus)
             @unrecoverable_exit = true
